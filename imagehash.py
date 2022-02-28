e97bfac982de85ef12f196d4c3a11103005dd51e
vitroid <vitroid@gmail.com>
2022-02-28 13:11:37 +0900

    Flatten DB. It is simpler and faster.

--- imagehash.py
+++ imagehash.py
@@ -1,20 +1,19 @@
 #
-# 画像を白黒にし、コントラストを最強にし、2x2d2にする。
-# その名前のディレクトリを掘り、そこに実体の場所を保存する。
-# また、実体の場所だけのデータベースを別に作り、そちらにはハッシュ内の場所を保存する。
+# ツリー状のデータベースでは保存しにくいので、平坦なデータベースにする。
+# 1つのthumbnailに対し複数のファイル、ひとつのファイルに対し複数のthumbnail
 # 
-# このprototypeでは、ディレクトリに実際に保存するのではなく、
-# メモリー上ですべて処理する。
+# このprototypeでは、ディレクトリに実際に保存するのではなく # メモリー上ですべて処理する。
 
 import cv2
 import numpy as np
+from collections import defaultdict
 
 class ImageHash():
     def __init__(self, maxlevel=8):
-        # file path to hash path
-        self.directory = dict()
-        # hash path to file path
-        self.tree = dict()
+        # file path to thumbnail hashes
+        self.thumbs = dict()
+        # thumbnail hash files
+        self.locations = defaultdict(list)
         self.maxlevel = maxlevel
     def coarsegrain(self, image, level=1):
         assert 0 < level < self.maxlevel+1
@@ -23,36 +22,23 @@ class ImageHash():
         tn = cv2.resize(cv2.cvtColor(image, cv2.COLOR_BGR2GRAY), (width, width)).astype(int)
         vmin = np.min(tn)
         vmax = np.max(tn)
-        return str(((tn-vmin) * depth // (vmax-vmin+1)).flatten())
+        return hash(tuple(((tn-vmin) * depth // (vmax-vmin+1)).flatten()))
     def query_by_image(self, image, similarity=8):
         if similarity > self.maxlevel:
             similarity = self.maxlevel
-        level = 1
-        branch = self.tree
-        while True:
-            tn = self.coarsegrain(image, level)
-            if tn in branch:
-                if level == similarity:
-                    # multiple hit
-                    return branch[tn]
-                level += 1
-                branch = branch[tn]
-            else:
-                return
-    def register(self, image, uniquename):
-        branch = self.tree
+        tn = self.coarsegrain(image, similarity)
+        return self.locations[tn]
+    def register(self, image, path):
+        thumbs = [0,]
         for level in range(1,self.maxlevel+1):
             tn = self.coarsegrain(image, level)
-            if tn not in branch:
-                branch[tn] = dict()
-            branch = branch[tn]
-        # the leave
-        # branch[uniquename] = image
-        branch[uniquename] = cv2.resize(image, (100,100))
-        # ここは、ツリー内でのデータ位置を保管してほしい。(上書きした時にはそれをたどって古いデータを消す。)
-        self.directory[uniquename] = tn
+            if tn not in self.locations:
+                self.locations[tn] = []
+            self.locations[tn].append(path)
+            thumbs.append(tn)
+        self.thumbs[path] = thumbs
     def query_by_name(self, name):
-        if name in self.directory:
-            return self.directory[name]
+        if name in self.thumbs:
+            return self.thumbs[name]
         return None
 
--- test.py
+++ test.py
@@ -4,12 +4,6 @@ import cv2
 import json
 import sys
 
-def images_iter(q):
-    for k in q:
-        if isinstance(q[k], dict):
-            yield from images_iter(q[k])
-        else:
-            yield k, q[k]
 
 
 # ih = ImageHashOnDisk("test", maxlevel=8)
@@ -25,13 +19,12 @@ for line in images:
     image = cv2.imread(name)
     # 似た画像をツリー構造で返す。
     # maxlevelを大きくするほど、細かい差を区別する。
-    q = ih.query_by_image(image, similarity=5)
-    if q is not None:
-        print(f"<h2>{name}</h2><p>")
-        for path, tn in images_iter(q):
-            print(f"<img src='file://{path}' width='100px' height='auto' />")
-        print("</p>")
+    print(f"<h2>{name}</h2><p>")
+    for similarity in range(6,2,-1):
+        print(f"<h3>Similarity {similarity}</h3><p>")
+        q = ih.query_by_image(image, similarity=similarity)
+        if q is not None:
+            for path in q:
+                print(f"<img src='file://{path}' width='100px' height='auto' />")
+    print("</p>")
 
-# めっちゃうまく動いている。
-# 次はストレージへの保管。単純に、pythonのツリー構造をそのままディスク上に展開できるmoduleはないか?
-# pickleのようなもので、disk上で動くもの。
